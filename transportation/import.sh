92d5a4cd49d1bc618fbf4a2d24d6585af20a756e
Justin Merz <jrmerz@gmail.com>
2016-03-14 10:39:17 -0700

    update for build, adding not about correction to conf file

--- transportation/import.sh
+++ transportation/import.sh
@@ -13,17 +13,25 @@ if [ ! -d "$TOOLDIR" ]; then
   wget http://osm2po.de/releases/osm2po-$RELEASE.zip
   unzip osm2po-$RELEASE.zip -d $TOOLDIR
   rm osm2po-$RELEASE.zip
+
+  echo "*******************************************"
+  echo "You now need to edit ./$TOOLDIR/osm2po.conf"
+  echo "Uncomment: "
+  echo "  'postp.0.class = de.cm.osm2po.plugins.postp.PgRoutingWriter'"
+  echo "  'postp.0.writeMultiLineStrings = true'"
+  echo ""
+  echo "Then re-run script"
+  exit 1;
 fi
 
 # prepare each state
 ALL=""
 for i in "${FILES[@]}"
 do
-  $EXEC cmd=c workDir=data/$i prefix=$i tileSize=x,c $DATAROOT$i-latest.osm.pbf
+  $EXEC cmd=tj workDir=data/$i prefix=$i tileSize=x,c $DATAROOT$i-latest.osm.pbf
   ALL=$ALL" data/"$i
 done
 
 # merge each state into $DEST.  fires up test server when completed
-$EXEC workDir=data/$DEST cmd=m tileSize=x,c prefix=$DEST $ALL
-$EXEC workDir=data/$DEST cmd=sg tileSize=x,c prefix=$DEST
+$EXEC workDir=data/$DEST cmd=mspg tileSize=x,c prefix=$DEST $ALL
 $EXEC workDir=data/$DEST cmd=r tileSize=x,c prefix=$DEST
